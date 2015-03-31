40a873dc68db855356ce0e1e63e975c031f6a398
Remi Louf <remilouf@gmail.com>
2015-03-31 17:22:22 +0100

    Save the results. Deal with encoding.

--- parser.py
+++ parser.py
@@ -10,8 +10,11 @@ from gzip import GzipFile
 __authors__ = """\n""".join(["Rémi Louf <remi.louf@scities>"])
 
 ## Feed a gzipped osm file
-path = "map.osm.gz"
+path = "data/brussels_belgium.osm.gz"
 
+#
+# Parse the OSM file
+#
 places = {}
 with open(path, "r") as c_file, GzipFile(fileobj=c_file) as xml_file:
     parser = et.iterparse(xml_file)
@@ -25,13 +28,24 @@ with open(path, "r") as c_file, GzipFile(fileobj=c_file) as xml_file:
             for child in elem: 
                 if child.tag == 'tag' and child.attrib != {}:
                     if child.attrib['k'] == 'name':
-                        name = child.attrib['v']
+                        name = unicode(child.attrib['v'],
+                                        'utf-8')
                     if child.attrib['k'] == 'place':
-                        place = child.attrib['v']
+                        place = unicode(child.attrib['v'],
+                                        'utf-8')
             if place:
                 if place not in places:
                     places[place] = {}
                 places[place][name] = {'lat':elem.attrib['lat'],
                                        'lon':elem.attrib['lon']} 
 
-print places
+
+#
+# Save the data
+#
+for place_type in places:
+    with open("extr/%s.csv"%place_type, "w") as output:
+        for name,coords in places[place_type].iteritems():
+            output.write("%s\t%s\t%s\n"%(name,
+                                        coords['lat'],
+                                        coords['lon']))
