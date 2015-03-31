5019a3de264882550ca554e6b5978b803d4832c9
Remi Louf <remilouf@gmail.com>
2015-03-31 17:02:17 +0100

    Deal with gzipped osm files

--- parser.py
+++ parser.py
@@ -5,32 +5,33 @@ Script to parse the planet.osm file (or any .osm file) to get a list of all citi
 villages in the worlds and their coordinates.
 """
 import xml.etree.cElementTree as et
-
+from gzip import GzipFile
 
 __authors__ = """\n""".join(["Rémi Louf <remi.louf@scities>"])
 
-
-path = "map.osm"
+## Feed a gzipped osm file
+path = "map.osm.gz"
 
 places = {}
-parser = et.iterparse(path)
-for action, elem in parser:
-    if elem.tag=="way":
-        elem.clear()
-    if elem.tag=="relation":
-        elem.clear()
-    if elem.tag=="node":
-        place = False 
-        for child in elem: 
-            if child.tag == 'tag' and child.attrib != {}:
-                if child.attrib['k'] == 'name':
-                    name = child.attrib['v']
-                if child.attrib['k'] == 'place':
-                    place = child.attrib['v']
-        if place:
-            if place not in places:
-                places[place] = {}
-            places[place][name] = {'lat':elem.attrib['lat'],
-                                   'lon':elem.attrib['lon']}
+with open(path, "r") as c_file, GzipFile(fileobj=c_file) as xml_file:
+    parser = et.iterparse(xml_file)
+    for action, elem in parser:
+        if elem.tag=="way":
+            elem.clear()
+        if elem.tag=="relation":
+            elem.clear()
+        if elem.tag=="node":
+            place = False 
+            for child in elem: 
+                if child.tag == 'tag' and child.attrib != {}:
+                    if child.attrib['k'] == 'name':
+                        name = child.attrib['v']
+                    if child.attrib['k'] == 'place':
+                        place = child.attrib['v']
+            if place:
+                if place not in places:
+                    places[place] = {}
+                places[place][name] = {'lat':elem.attrib['lat'],
+                                       'lon':elem.attrib['lon']} 
 
 print places
